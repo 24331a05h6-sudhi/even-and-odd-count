f3237b8981de27825aff2f1953c6625f1770ff2d
24331a05h6-sudhi <24331a05h6@mvgrce.edu.in>
2026-03-29 08:36:59 +0530

    Add files via upload

---
+++ count.py
@@ -0,0 +1,15 @@
+n = int(input("Enter the range: "))
+
+even_count = 0
+odd_count = 0
+
+for i in range(1, n + 1):
+    if i % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+
+print("Even numbers:", even_count)
+print("Odd numbers:", odd_count)
+
+
